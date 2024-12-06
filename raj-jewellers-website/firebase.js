8812847c2491250cc4187db845dd9809489396e8
Gunan-Jain <gunanjain809@gmail.com>
2024-12-07 00:39:29 +0530

    Adding firebase.js

---
+++ raj-jewellers-website/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC6HXZFoEGWnLdI5eaEfE-GCLvCnW-zgKU",
+    authDomain: "raj-jewellers-demo-website.firebaseapp.com",
+    projectId: "raj-jewellers-demo-website",
+    storageBucket: "raj-jewellers-demo-website.firebasestorage.app",
+    messagingSenderId: "955613170189",
+    appId: "1:955613170189:web:cb04cef76d9673652c2344",
+    measurementId: "G-SM0Y3HM47R"
+  };
